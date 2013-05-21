b8f034091770ef369e203eb40032a28ff2d5e962
Piotr Zaw <piotrek.gda24@gmail.com>
2013-05-21 23:04:37 +0200

    dfbgn gfffn g

---
+++ Blog.DAL.Tests/RepositoryTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Blog.DAL.Infrastructure;
+using Blog.DAL.Model;
+using Blog.DAL.Repository;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+using TDD.DbTestHelpers.Yaml;
+using TDD.DbTestHelpers.Core;
+
+namespace Blog.DAL.Tests
+{
+    [TestClass]
+    public class RepositoryTests : DbBaseTest<BlogFixtures>
+    {
+        [TestMethod]
+        public void GetAllPost_OnePostInDb_ReturnOnePost()
+        {
+            // arrange
+            //var context = new BlogContext();
+            //context.Database.CreateIfNotExists();
+            var repository = new BlogRepository();
+
+            //context.Posts.ToList().ForEach(x => context.Posts.Remove(x));
+            //context.Posts.Add(new Post { Author = "test", Content = "test, test, test..." });
+            //context.SaveChanges();
+
+            // act
+            var result = repository.GetAllPosts();
+            // assert
+            Assert.AreEqual(1, result.Count());
+        }
+
+        [TestMethod]
+        public void AddPost_OnePostInDb_ReturnCorrectNumberOfPosts()
+        {
+            var repository = new BlogRepository();
+
+            var post = new Post { Author = "aaa", Content = "xyz" };
+            repository.AddPost(post);
+            var result = repository.GetAllPosts();
+
+            Assert.AreEqual(2, result.Count());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void AddPost_WithoutRequiredData_ThrowsException()
+        {
+            var repository = new BlogRepository();
+            var post = new Post { Author = null, Content = "xyz" };
+            repository.AddPost(post);
+        }
+    }
+
+    public class BlogFixtures : YamlDbFixture<BlogContext, BlogFixturesModel>
+    {
+        public BlogFixtures()
+        {
+            SetYamlFiles("posts.yml");
+        }
+    }
+
+    public class BlogFixturesModel
+    {
+        public FixtureTable<Post> Posts { get; set; }
+    }
+
+}
---
+++ Blog.DAL/Repository/BlogRepository.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Blog.DAL.Infrastructure;
+using Blog.DAL.Model;
+using System;
+
+namespace Blog.DAL.Repository
+{
+    public class BlogRepository
+    {
+        private readonly BlogContext _context;
+
+        public BlogRepository()
+        {
+            _context = new BlogContext();
+        }
+
+        public IEnumerable<Post> GetAllPosts()
+        {
+            return _context.Posts;
+        }
+
+        public void AddPost(Post post)
+        {
+            if (post.Author == null)
+                throw new ArgumentException("Author");
+            _context.Posts.Add(post);
+            _context.SaveChanges();
+        }
+    }
+}
